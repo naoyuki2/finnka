afea6ba16bb97b842460297e23d62ad8b2076a7a
Hiroshi <137484523+Hiroshi7700@users.noreply.github.com>
2023-10-25 14:59:36 +0900

    アカウント削除画面1

---
+++ PHP/account deletion 1.php
@@ -0,0 +1,16 @@
+<?php
+    require './common/header.php';
+    require './common/db-connect.php';
+
+echo '<form action="account deletion 2.php" method="post">';
+    echo '<p>アカウント削除1</p>';
+    echo "<h1>アカウント削除</h1>";
+    echo "<p>注意</p>";
+    echo "<p>アカウントを削除するとすべての<br>";
+    echo "登録データが削除されます";
+    echo "<br>";
+    echo '<button type="submit">キャンセル</button>';
+    echo '<button type="submit">削除</button>';
+echo '</form>';
+     require './common/footer.php';
+?>
