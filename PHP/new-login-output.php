f7d486c98f847c87e20c72fe92336d93a7d84daf
naoyuki <kamaboko912@gmail.com>
2023-11-01 14:49:02 +0900

    login,newlogin,comp

--- PHP/new-login-output.php
+++ PHP/new-login-output.php
@@ -1,35 +1,44 @@
-<?php session_start(); ?>
-<?php require './common/header.php';?>
-<?php require './common/db-connect.php'; ?>
 <?php
-    $password_hash = password_hash($_POST['password'],PASSWORD_DEFAULT);
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-    $pdo = new PDO ($connect,USER,PASS);
-    if(isset($_SESSION['user'])){
-        $user_id=$_SESSION['user']['user_id'];
-        $sql=$pdo->prepare('select * from user where user_id!=? and user_name=?');
-        $sql->execute([$user_id,$_POST['user_name']]);
-    }else{
-        $sql=$pdo->prepare('select * from user where user_name=?');
-        $sql->execute([$_POST['user_name']]);
+require './common/db-connect.php';
+
+$salt = substr(base_convert(hash('sha256', uniqid()), 16, 36), 0, 20);
+$password_hash = password_hash($_POST['password'].$salt,PASSWORD_DEFAULT);
+
+$pdo = new PDO ($connect,USER,PASS);
+if(isset($_SESSION['user'])){
+    $user_id=$_SESSION['user']['user_id'];
+    $sql=$pdo->prepare('select * from user where user_id!=? and user_name=?');
+    $sql->execute([$user_id,$_POST['user_name']]);
+}else{
+    $sql=$pdo->prepare('select * from user where user_name=?');
+    $sql->execute([$_POST['user_name']]);
+}
+
+if(empty($sql->fetchAll())){
+    if($_POST['password'] !== $_POST['password2']) {
+        $_SESSION['error_message'] = 'パスワードが一致しません。';
+        header('Location: new-login-input.php');
+        exit;
+    } else if (empty($_POST['password']) || empty($_POST['password2'])) {
+        $_SESSION['error_message'] = 'パスワードを入力してください';
+        header('Location: new-login-input.php');
+        exit;
+    } else {
+        $sql=$pdo->prepare('insert into user values(null,?,0,0,?,?)');
+        $sql->execute([
+        $_POST['user_name'],$password_hash,$salt]);
+        $_SESSION['id'] = $sql['user_id'];
+        header('Location: top.php');
+        exit;
     }
+}else{
+    $_SESSION['error_message'] = 'ログイン名がすでに使用されていますので、変更してください。';
+    header('Location: new-login-input.php');
+    exit;
+}
 
-        $sql->fetchAll();
-
-        if($_POST['password'] !== $_POST['password2']) {
-            echo 'パスワードが一致しません。';
-            echo '<form action="new-login-input.php" method="post">';
-            echo '<input type="submit" value="戻る">';
-            echo '</form>';
-        }else{
-            $sql=$pdo->prepare('insert into user values(null,?,?,0,0)');
-            $sql->execute([
-                $_POST['user_name'],$password_hash]);
-            echo'お客様情報を登録しました。';
-            echo '<form action="top.php" method="post">';
-            echo '<input type="submit" value="トップページへ">';
-            echo '</form>';
-        }
-
-require './common/footer.php';
 ?>
