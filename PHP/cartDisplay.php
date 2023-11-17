3fa312f8d220a255d8cb09b3881d30f3e890a663
shion <2201376@s.asojuku.ac.jp>
2023-11-17 14:43:03 +0900

    カート小計

--- PHP/cartDisplay.php
+++ PHP/cartDisplay.php
@@ -2,16 +2,22 @@
     require './common/header.php';
     require './common/db-connect.php';
     $pdo = new PDO($connect,USER,PASS);
+
     
+
     $sql=$pdo->prepare('select * from cart where user_id = ?');
     $sql->execute([$_SESSION['id']]);
     $cart_id = $sql->fetchColumn();//cart_id:10
+
+    $count = 0;
+    $totalPrice = 0;
 
     $cartDetail=$pdo->prepare('select * from cartDetails where cart_id = ?');
     $cartDetail->execute([$cart_id]);
     $cartDetail_result = $cartDetail->fetchAll();
     echo '<div class="container">';
     echo '<div class="row">';
+    
     if(!empty($cartDetail_result)){
         foreach($cartDetail_result as $row){
             $product=$pdo->prepare('select * from products where product_id = ?');
@@ -22,6 +28,20 @@
             $stmt->execute([$result['author_id']]);
             $author = $stmt->fetch();
 
+                $product_id = $row['product_id'];
+                $quantity = $row['quantity'];
+
+                $product_sql = $pdo->prepare("SELECT * FROM products WHERE product_id = ?");
+      $product_sql->execute([$product_id]);
+      $product_row = $product_sql->fetch();
+
+      $author_sql = $pdo->prepare('SELECT author_name FROM author WHERE author_id = ?');
+      $author_sql->execute([$product_row['author_id']]);
+      $author_row = $author_sql->fetch();
+
+      $count += $quantity;
+      $totalPrice += $quantity * $product_row['price'];
+  
                 echo '<div class="col-12 col-md-6">';
                     echo '<div class="card">';
                         echo '<div class="row g-0">';
@@ -49,12 +69,20 @@
                 echo '</div>';
 
         }
+
+      echo '<div class="container text-center">'; 
+      echo '<div class="mb-3"></div>'; 
+    echo '<h4>小計 ', $totalPrice, ' 円</h4>';
+
+        echo '</div>';
         echo '</div>';
         echo '</div>';
         echo '<div class="container text-center">';
         echo '<div class="row">';
         echo '<div class="mb-3"></div>';
-        echo '<a href="orderInput.php"><button type="button" class="btn btn-success">購入する</button></a>';
+
+        echo '<a href="orderInput.php"><button type="button" class="btn btn-success">',$count,'点 購入する</button></a>';
+    
         echo '</div>';
         echo '</div>';
     }else{
