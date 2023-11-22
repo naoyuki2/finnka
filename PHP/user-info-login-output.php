d67822fb8c4f68e94b73dc1dac9412bebd3aae0b
shion <2201376@s.asojuku.ac.jp>
2023-11-22 14:44:12 +0900

    認証デザイン

--- PHP/user-info-login-output.php
+++ PHP/user-info-login-output.php
@@ -1,34 +1,41 @@
-<?php session_start(); ?>
-<?php require './common/header.php'; ?>
-<?php require './common/db-connect.php'; ?>
-
 <?php
-unset($_SESSION['user']);
-$pdo = new PDO($connect, USER, PASS);
-$sql = $pdo->prepare('select * from user where user_name=?');
-$sql->execute([$_POST['user_name']]);
-$row = $sql->fetch(); // 一致するユーザーが1つだけであることを仮定
+session_start();
 
-if ($row && password_verify($_POST['password'], $row['password'])) {
-    $_SESSION['user'] = [
-        'user_id' => $row['user_id'],
-        'user_name' => $row['user_name'],
-        'password' => $row['password'],
-        'icon_color' => 0,
-        'delete_flg' => 0
-    ];
+require './common/db-connect.php';
 
-    echo 'おかえりなさい、', $_SESSION['user']['user_name'], 'さん。';
-    echo '<form action="user-info-fenkou.php" method="post">';
-    echo '<input type="submit" value="ユーザー変更">';
-    echo '</form>';
-} else {
-    echo 'ログイン名またはパスワードが違います。';
-    echo '<form action="user-info-login-input.php" method="post">';
-    echo '<input type="submit" value="戻る">';
-    echo '</form>';
+$errorMessage = "";
+
+$pdo = new PDO ($connect,USER,PASS);
+// ユーザー認証の処理
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $userName = $_POST['user_name'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    // データベースでのユーザー情報の取得
+    $sql = $pdo->prepare("SELECT * FROM user WHERE user_name = ?");
+    $sql->execute([$userName]);
+    $row = $sql->fetch();
+
+    if ($row) {
+        // パスワードの検証
+        $passwordInput = $password . $row['salt'];
+        if (password_verify($passwordInput, $row['hash'])) {
+            // 認証成功: ユーザー情報をセッションに保存
+            $_SESSION['user_name'] = $userName;
+
+            // i.phpにリダイレクト
+            header('Location: userhenkou-input.php');
+            exit;
+        } else {
+            // 認証失敗: エラーメッセージをセッションに保存
+            $_SESSION['user-info-error'] = 'ログイン名またはパスワードが違います。';
+            header('Location: user-info-login-input.php');
+            exit;
+        }
+    } else {
+        $_SESSION['user-info-error'] = 'ユーザーが見つかりません。';
+        header('Location: user-info-login-input.php');
+        exit;
+    }
 }
-
 ?>
-
-<?php require './common/footer.php'; ?>
