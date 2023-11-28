ef3a80bac2044c35a9758eb3859ae2389b9957ef
shion <2201376@s.asojuku.ac.jp>
2023-11-28 12:34:36 +0900

    ヘッダー

--- PHP/admin-login-input.php
+++ PHP/admin-login-input.php
@@ -1,6 +1,33 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="../CSS/style.css" />
+
+    <title>finnka</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container-fluid">
+            <img name="logo" src="../CSS/finnkaLogo.jpg" alt="logo">
+        </div>
+        <a href="login-input.php">
+          ログイン画面に戻る
+        </a>
+    </nav>
 <?php
-session_start();
-require './common/login-header.php';
+
 require './common/db-connect.php';
 $errorMessage = "";
 if (isset($_SESSION['errorMessage'])) {
